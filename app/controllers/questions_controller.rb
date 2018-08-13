791cf16ac5018c142e5eef23d7c9786f44027256
cory bonneau <cory.bonneau2424@gmail.com>
2018-08-13 15:07:00 +0200

    finished challenge

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# commetn
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params['question'].downcase
+    @answer = if @question == 'I am going to work'
+                'Great!'
+              elsif @question.include? '?'
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+  end
+end
